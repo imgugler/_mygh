02022e0fc464410c94749190a0a46b490d12a970
webpyobum <imgugler@gmail.com>
2015-12-06 14:48:39 +0900

    .
    .

---
+++ blog/writer/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php include('data/title.txt'); ?></title>
+<link rel="stylesheet" href="../static/css/reset.css" />
+<link rel="stylesheet" href="../static/css/style.css" />
+</head>
+<body>
+
+
+ 
+
+
+<div class="main">
+	<article class="article">
+		<div class="obj">
+			<h1 class="">
+				<?php include('data/title.txt'); ?>
+			</h1>
+				<?php include('data/text.txt'); ?>
+			<p class="date">
+				<span class="die">작성일:</span>
+				<?php include('data/date.txt'); ?>
+			</p>	
+		</div>
+	</article>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
